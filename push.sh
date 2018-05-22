e09481f444462085c9ae4c974334bd92aa1df323
lrsafari <lrsafari@gmail.com>
2018-05-22 12:50:56 -0700

    made changes

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+###########################
+cd C:\Users\lrsaf\OneDrive\Documents\_Spring_2018\CIS_233DA_Advanced_Web_Dev\sMIller-CIS233DA-0300_052218
+# switch to branch you want to use
+git checkout master
+# add all added/modified files
+git add .
+# commit changes
+git commit -am "made changes"
+# push to git remote repository
+git push
+###########################
+echo Press Enter...
+read
